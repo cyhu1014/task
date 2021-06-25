f38bbf0cb4a6c195429c06267ac039953e10d2a9
ryderhu <kayuuko@jiayouhude-MacBook-Pro.local>
2021-06-25 14:23:10 +0800

    use hashlib

--- segtomd5.py
+++ segtomd5.py
@@ -1,11 +1,12 @@
 import os
-from md5 import MD5
 import sqlite3
+import hashlib
 con = sqlite3.connect('example.db')
 cur = con.cursor()
 # Create table
 cur.execute('''CREATE TABLE chunk
                (data_name text, data_size int, md5_hash text)''')
+m = hashlib.md5()
 
 abs_path = "seg/"
 seg_list = sorted(os.listdir(abs_path))
@@ -17,8 +18,9 @@ for i in seg_list:
     f = open(abs_path+i,"rb")
     file = f.read()
     file_size = len(file)
-    string1 = str(file)
-    md5_hash = MD5.hash(string1)
+    string1 = str(file).encode("utf-8")
+    m.update(string1)
+    md5_hash = m.hexdigest()
     print(f_name,file_size,md5_hash)
     cur.execute("INSERT INTO chunk VALUES ('%s','%d','%s')"%(f_name,file_size,md5_hash))
     con.commit()
